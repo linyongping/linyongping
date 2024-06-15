1bfbe4dec3507d87e25a0e14ff4e19fb45d7602f
Yongping Lin <yongping.lin@compass.com>
2024-06-15 23:30:31 +0800

    init comment

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,72 @@
 import Image from "next/image";
 
-export default function Home() {
+async function getData(): Promise<{
+  monthly_bw_limit_b: number;
+  bw_counter_b: number;
+  bw_reset_day_of_month: number;
+}> {
+  const res = await fetch(
+    "https://justmysocks6.net/members/getbwcounter.php?service=1005699&id=60886f48-f5d7-4787-87af-5f172b056cfe"
+  );
+  return res.json();
+}
+
+const ProgressBar = ({
+  total = 0,
+  progress = 0,
+}: {
+  total?: number;
+  progress?: number;
+}) => {
+  // Calculate the width percentage of the progress
+  const progressPercentage = (progress / total) * 100;
+
+  const progressBarStyle = {
+    width: `${progressPercentage}%`,
+    height: '100%',
+    background: progressPercentage > 85 
+      ? 'linear-gradient(90deg, red, darkred)'
+      : 'linear-gradient(90deg, #00ffff, #007f7f)',
+    boxShadow: progressPercentage > 85
+      ? '0 0 10px red, 0 0 20px red, 0 0 30px red'
+      : '0 0 10px #00ffff, 0 0 20px #00ffff, 0 0 30px #00ffff'
+  };
+
+  return (
+    <div className="w-full bg-black rounded overflow-hidden h-5 border border-gray-600">
+      <div
+        className="h-full bg-cyan-400"
+        style={progressBarStyle}
+      ></div>
+    </div>
+  );
+};
+
+const GB = 1000 * 1000 * 1000;
+
+// To calculate how many days are left from the current day to the next month's specific day using JavaScript in a browser environment,
+function getDaysLeft(specificDay: number) {
+  const today = new Date();
+  const nextMonth = new Date(
+    today.getFullYear(),
+    today.getMonth() + 1,
+    specificDay
+  );
+  const timeLeft = nextMonth.getTime() - today.getTime();
+  const daysLeft = Math.ceil(timeLeft / (1000 * 60 * 60 * 24));
+  return daysLeft;
+}
+
+export default async function Home() {
+  const data = await getData();
+  const totalData = data.monthly_bw_limit_b / GB;
+  const usedData = data.bw_counter_b / GB;
+  const resetDay = data.bw_reset_day_of_month;
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"></p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -39,74 +98,13 @@ export default function Home() {
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="mb-32 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:text-left">
+        <p>Total: {totalData} GB</p>
+        <p>Used: {usedData.toFixed(2)} GB</p>
+        <p>Remaining: {(totalData - usedData).toFixed(2)} GB</p>
+        <p>Reset day: {resetDay} th</p>
+        <p>Left days to next reset: {getDaysLeft(resetDay)} days</p>
+        <ProgressBar total={totalData} progress={usedData} />
       </div>
     </main>
   );
