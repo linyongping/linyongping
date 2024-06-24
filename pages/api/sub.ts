47204a412ca3acf27fa065ca8d772afd7229c92b
Yongping Lin <yongping.lin@compass.com>
2024-06-25 00:45:33 +0800

    fix errro

--- pages/api/sub.ts
+++ pages/api/sub.ts
@@ -3,6 +3,10 @@ import fse from "fs-extra";
 import yaml from "js-yaml";
 import { NextApiRequest, NextApiResponse } from "next";
 
+export const config = {
+  runtime: "edge",
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -14,7 +18,7 @@ export default async function handler(
     const tempFile = await fse.readFile(filePath, "utf8");
     const json = JSON.stringify(yaml.load(tempFile));
     res.setHeader("Content-Type", "text/plain");
-    res.setHeader('x-timestamp', currentTime);
+    res.setHeader("x-timestamp", currentTime);
     res.status(200).send(json);
   } catch (err) {
     console.log(err);
--- pages/sub.tsx
+++ pages/sub.tsx
@@ -1,20 +1,8 @@
-"use client";
-
-import { useQuery } from "@tanstack/react-query";
-
 export default function Sub() {
-  const { data, isLoading } = useQuery({
-    queryKey: ["sub"],
-    queryFn: () => {
-      return fetch("/api/sub").then((res) => res.json());
-    },
-  });
-
   return (
     <div>
       <h1>Sub Page</h1>
-      {isLoading ? <div>Loading...</div> : null}
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <pre>SUB</pre>
     </div>
   );
 }
